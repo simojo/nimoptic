828458c25ad7a11720437d6abc82ce4669589c4a
Simon Jones <github@simonjjones.com>
2021-04-19 23:13:25 -0400

    setting up project structure

---
+++ src/nimoptic/constants.nim
@@ -0,0 +1,4 @@
+## speed of light (m.s^-1)
+const c*: float   = 299792458
+## e naught (F.m^-1)
+const e0*: float  = 8.85418781762e-12
---
+++ src/nimoptic/lens.nim
@@ -0,0 +1,7 @@
+type
+  ## Lens object
+  lens* = object
+    ## Focal length of the lens
+    focalLength*: float
+    ## Diameter of the lens
+    diameter*: float
